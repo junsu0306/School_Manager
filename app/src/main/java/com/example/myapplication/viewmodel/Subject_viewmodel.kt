7b5c6ff6b1f3afe52174c347b429d1455d021b81
junsu0306 <76641820+junsu0306@users.noreply.github.com>
2023-11-04 01:50:15 +0900

    change

---
+++ app/src/main/java/com/example/myapplication/viewmodel/Subject_viewmodel.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+
+data class Course(val courseName: String, val grade: String, val credits: Int)
+
+class Subject_viewmodel {
+
+}
